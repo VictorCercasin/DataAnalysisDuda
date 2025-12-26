d5b1d3913e7a9271ad860e6c8f5a52104a72d20b
Victor Hugo Cercasin <victorhugo.cercasin@gmail.com>
2025-12-26 10:24:03 -0300

    Inicio da análise macroscópica

--- extracao.py
+++ extracao.py
@@ -7,9 +7,10 @@ from openpyxl import load_workbook
 from src.load_data import  Extrator
 
 
-FILE_PATH_CANDIDA_FISICOQUIMICO = Path('data/etapa_2/Candida/Candida.xlsx')
-FILE_PATH_ASPERGILLIUS_FISICOQUIMICO = Path('data/etapa_2/Aspergillius/Aspergillius.xlsx')
-FILE_PATH_SACCHAROMYCES_FISICOQUIMICO = Path('data/etapa_2/Saccharomyces/Saccharomyces.xlsx')
+FILE_PATH_CANDIDA_FISICOQUIMICO = Path('data/etapa_2/Candida/Fisico-Quimico.xlsx')
+FILE_PATH_ASPERGILLIUS_FISICOQUIMICO = Path('data/etapa_2/Aspergillius/Fisico-Quimico.xlsx')
+FILE_PATH_SACCHAROMYCES_FISICOQUIMICO = Path('data/etapa_2/Saccharomyces/Fisico-Quimico.xlsx')
+FILE_PATH_PENICILLIUM_FISICOQUIMICO = Path('data/etapa_2/Penicillium/Fisico-Quimico.xlsx')
 
 FILE_PATH_CANDIDA_METAIS_TOXICOS = Path('data/etapa_2/Análise Metais.xlsx')
 FILE_PATH_ASPERGILLIUS_METAIS_TOXICOS = Path('data/etapa_2/Análise Metais.xlsx')
@@ -17,41 +18,52 @@ FILE_PATH_SACCHAROMYCES_METAIS_TOXICOS = Path('data/etapa_2/Análise Metais.xlsx
 FILE_PATH_PENICILLIUM_METAIS_TOXICOS = Path('data/etapa_2/Análise Metais.xlsx')
 
 
+FILE_PATH_SACCHAROMYCES_ANALISE_MACROSCOPICA = Path('data/etapa_2/Saccharomyces/Análise Macroscópica.xlsx')
+
 def main():
     todas_amostras = []
 
-    # Extração fisicoquímico
-    extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_CANDIDA_FISICOQUIMICO, sheet='PROCESSAMENTO')
-    extrator_candida_fisicoquimoco.extrair_fisicoquimico()
-    todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
+    # # Extração fisicoquímico
+    # extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_CANDIDA_FISICOQUIMICO, sheet='PROCESSAMENTO', fungo="Candida")
+    # extrator_candida_fisicoquimoco.extrair_fisicoquimico()
+    # todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
 
-    extrator_aspergillus_fisicoquimoco = Extrator(file_path=FILE_PATH_ASPERGILLIUS_FISICOQUIMICO, sheet='PROCESSAMENTO')
-    extrator_aspergillus_fisicoquimoco.extrair_fisicoquimico()
-    todas_amostras = todas_amostras + extrator_aspergillus_fisicoquimoco._amostras
+    # extrator_aspergillus_fisicoquimoco = Extrator(file_path=FILE_PATH_ASPERGILLIUS_FISICOQUIMICO, sheet='PROCESSAMENTO', fungo="Aspergillius")
+    # extrator_aspergillus_fisicoquimoco.extrair_fisicoquimico()
+    # todas_amostras = todas_amostras + extrator_aspergillus_fisicoquimoco._amostras
 
-    extrator_saccharomyces_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_FISICOQUIMICO, sheet='PROCESSAMENTO')
-    extrator_saccharomyces_fisicoquimoco.extrair_fisicoquimico()
-    todas_amostras = todas_amostras + extrator_saccharomyces_fisicoquimoco._amostras
+    # extrator_saccharomyces_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_FISICOQUIMICO, sheet='PROCESSAMENTO', fungo="Saccharomyces")
+    # extrator_saccharomyces_fisicoquimoco.extrair_fisicoquimico()
+    # todas_amostras = todas_amostras + extrator_saccharomyces_fisicoquimoco._amostras
 
-    # Extração metais tóxicos
+    # extrator_saccharomyces_fisicoquimoco = Extrator(file_path=FILE_PATH_PENICILLIUM_FISICOQUIMICO, sheet='PROCESSAMENTO', fungo="Penicillium")
+    # extrator_saccharomyces_fisicoquimoco.extrair_fisicoquimico()
+    # todas_amostras = todas_amostras + extrator_saccharomyces_fisicoquimoco._amostras
 
-    extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_CANDIDA_METAIS_TOXICOS, sheet='Candida', fungo='Candida')
-    extrator_candida_fisicoquimoco.extrair_metais_toxicos()
-    todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
+    # # Extração metais tóxicos
 
-    extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_ASPERGILLIUS_METAIS_TOXICOS, sheet='Aspergillius', fungo='Aspergillius')
-    extrator_candida_fisicoquimoco.extrair_metais_toxicos()
-    todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
+    # extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_CANDIDA_METAIS_TOXICOS, sheet='Candida', fungo='Candida')
+    # extrator_candida_fisicoquimoco.extrair_metais_toxicos()
+    # todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
 
-    extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_METAIS_TOXICOS, sheet='Scharomicys', fungo='Saccharomyces')
-    extrator_candida_fisicoquimoco.extrair_metais_toxicos()
-    todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
+    # extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_ASPERGILLIUS_METAIS_TOXICOS, sheet='Aspergillius', fungo='Aspergillius')
+    # extrator_candida_fisicoquimoco.extrair_metais_toxicos()
+    # todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
 
-    extrator_penicillium_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_METAIS_TOXICOS, sheet='Penicillium', fungo='Penicillium')
-    extrator_penicillium_fisicoquimoco.extrair_metais_toxicos()
-    todas_amostras = todas_amostras + extrator_penicillium_fisicoquimoco._amostras
+    # extrator_candida_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_METAIS_TOXICOS, sheet='Scharomicys', fungo='Saccharomyces')
+    # extrator_candida_fisicoquimoco.extrair_metais_toxicos()
+    # todas_amostras = todas_amostras + extrator_candida_fisicoquimoco._amostras
+
+    # extrator_penicillium_fisicoquimoco = Extrator(file_path=FILE_PATH_SACCHAROMYCES_METAIS_TOXICOS, sheet='Penicillium', fungo='Penicillium')
+    # extrator_penicillium_fisicoquimoco.extrair_metais_toxicos()
+    # todas_amostras = todas_amostras + extrator_penicillium_fisicoquimoco._amostras
+    # todas_amostras = todas_amostras + extrator_penicillium_fisicoquimoco._amostras
 
 
+    # Extração análise macroscópica
+    extrator_saccharomyces_analise_macroscopica = Extrator(file_path=FILE_PATH_SACCHAROMYCES_ANALISE_MACROSCOPICA, sheet='Dados prontos Dia 0', fungo="Saccharomyces")
+    extrator_saccharomyces_analise_macroscopica.extrair_analise_macroscopica()
+    todas_amostras = todas_amostras + extrator_saccharomyces_analise_macroscopica._amostras
     df = pd.DataFrame([asdict(a) for a in todas_amostras])
 
     # Ensure output directory exists
--- src/load_data.py
+++ src/load_data.py
@@ -11,20 +11,20 @@ from openpyxl.utils import get_column_letter, column_index_from_string
 @dataclass
 class Amostra:
     fungo: str
-    sistema: Literal['AI','A','BI','B']
+    sistema: Literal['AI', 'A', 'BI', 'B', 'CN', 'CP']
     dia: Literal[0, 7, 14]
-    concentracao: Literal[0, 25, 50, 75, 100]
+    concentracao: Literal[0, 25, 50, 75, 100, -1]
     analise: str
     valor: Any
     
     def __post_init__(self):
-        valid_concentrations = {0, 25, 50, 75, 100}
+        valid_concentrations = {0, 25, 50, 75, 100, -1}
         if self.concentracao not in valid_concentrations:
             raise ValueError(f"concentracao deve ser um dos valores a seguir: {valid_concentrations}")
         valid_days = {0, 7, 14}
         if self.dia not in valid_days:
             raise ValueError(f"dia deve ser um dos valores a seguir: {valid_days}")
-        valid_systems = {'AI','A','BI','B'}
+        valid_systems = {'AI', 'A', 'BI', 'B', 'CN', 'CP'}
         if self.sistema not in valid_systems:
             raise ValueError(f"sistema deve ser um dos valores a seguir: {valid_systems}")
 
@@ -34,56 +34,135 @@ class Extrator:
     def __init__(self, file_path: str, sheet: str, fungo: str = ''):
         print("Iniciando extrator")
         self.file_path = file_path
-        self.wb = load_workbook(file_path, data_only=True)
         self.fungo = fungo if len(fungo) > 0 else self.file_path.stem
-        self.ws= self.wb[sheet]
         self._amostras: list[Amostra] = []
+        if os.path.exists(file_path):
+            self.wb = load_workbook(file_path, data_only=True)
+        else:
+            print(f"ERRO - Caminho {file_path} não existe")
+            return []
+        self.ws= self.wb[sheet]
 
 
-    # def extrair(self):
-        # self.add_amostra("AI", 7, 25, "pH", 3.5)
-        # print("Iniciando extração")
+    def extrair_analise_macroscopica(self):
+        # self.extrair_coluna(start_col_letter='d', start_row=4, dia=0, sistema='AI', analise='Sementes Germinadas')
+        # self.extrair_coluna(start_col_letter='d', start_row=9, dia=0, sistema='A', analise='Sementes Germinadas')
+        # self.extrair_coluna(start_col_letter='d', start_row=14, dia=0, sistema='BI', analise='Sementes Germinadas')
+        # self.extrair_coluna(start_col_letter='d', start_row=19, dia=0, sistema='B', analise='Sementes Germinadas')
 
-        # print("Extraíndo Fisico Químico")
-        # self.extrair_fisicoquimico()
-        # self.extrair_metais_toxicos()
+        # self.extrair_celula(start_col_letter='d', start_row=24, dia=0, sistema='CN', analise='Sementes Germinadas', concentracao=-1)
+        # self.extrair_celula(start_col_letter='d', start_row=25, dia=0, sistema='CP', analise='Sementes Germinadas', concentracao=-1)
+        self.extrair_celula(start_col_letter='d', start_row=12, dia=0, sistema='A', analise='Sementes Germinadas', concentracao=75)
 
+        # self._amostras = [
+        #     a for a in self._amostras
+        #     if a.valor != '#DIV/0!'
+        # ]
     def extrair_metais_toxicos(self):
-        self.extrair_coluna(start_col_letter='d', start_row=5, dia=0, sistema='AI', analise='Cd')
-        self.extrair_coluna(start_col_letter='d', start_row=10, dia=0, sistema='A', analise='Cd')
-        self.extrair_coluna(start_col_letter='d', start_row=15, dia=0, sistema='BI', analise='Cd')
-        self.extrair_coluna(start_col_letter='d', start_row=20, dia=0, sistema='B', analise='Cd')
 
-        self.extrair_coluna(start_col_letter='e', start_row=5, dia=0, sistema='AI', analise='Cr')
-        self.extrair_coluna(start_col_letter='e', start_row=10, dia=0, sistema='A', analise='Cr')
-        self.extrair_coluna(start_col_letter='e', start_row=15, dia=0, sistema='BI', analise='Cr')
-        self.extrair_coluna(start_col_letter='e', start_row=20, dia=0, sistema='B', analise='Cr')
+        # Apenas as concentrações do zinco ultrapassam os mínimos de sensibilidade do equipamento
 
-        self.extrair_coluna(start_col_letter='f', start_row=5, dia=0, sistema='AI', analise='Cu')
-        self.extrair_coluna(start_col_letter='f', start_row=10, dia=0, sistema='A', analise='Cu')
-        self.extrair_coluna(start_col_letter='f', start_row=15, dia=0, sistema='BI', analise='Cu')
-        self.extrair_coluna(start_col_letter='f', start_row=20, dia=0, sistema='B', analise='Cu')
+        # Dia 0
+        # self.extrair_coluna(start_col_letter='d', start_row=5, dia=0, sistema='AI', analise='Cd')
+        # self.extrair_coluna(start_col_letter='d', start_row=10, dia=0, sistema='A', analise='Cd')
+        # self.extrair_coluna(start_col_letter='d', start_row=15, dia=0, sistema='BI', analise='Cd')
+        # self.extrair_coluna(start_col_letter='d', start_row=20, dia=0, sistema='B', analise='Cd')
 
-        self.extrair_coluna(start_col_letter='g', start_row=5, dia=0, sistema='AI', analise='Ni')
-        self.extrair_coluna(start_col_letter='g', start_row=10, dia=0, sistema='A', analise='Ni')
-        self.extrair_coluna(start_col_letter='g', start_row=15, dia=0, sistema='BI', analise='Ni')
-        self.extrair_coluna(start_col_letter='g', start_row=20, dia=0, sistema='B', analise='Ni')
+        # self.extrair_coluna(start_col_letter='e', start_row=5, dia=0, sistema='AI', analise='Cr')
+        # self.extrair_coluna(start_col_letter='e', start_row=10, dia=0, sistema='A', analise='Cr')
+        # self.extrair_coluna(start_col_letter='e', start_row=15, dia=0, sistema='BI', analise='Cr')
+        # self.extrair_coluna(start_col_letter='e', start_row=20, dia=0, sistema='B', analise='Cr')
 
-        self.extrair_coluna(start_col_letter='h', start_row=5, dia=0, sistema='AI', analise='Pb')
-        self.extrair_coluna(start_col_letter='h', start_row=10, dia=0, sistema='A', analise='Pb')
-        self.extrair_coluna(start_col_letter='h', start_row=15, dia=0, sistema='BI', analise='Pb')
-        self.extrair_coluna(start_col_letter='h', start_row=20, dia=0, sistema='B', analise='Pb')
+        # self.extrair_coluna(start_col_letter='f', start_row=5, dia=0, sistema='AI', analise='Cu')
+        # self.extrair_coluna(start_col_letter='f', start_row=10, dia=0, sistema='A', analise='Cu')
+        # self.extrair_coluna(start_col_letter='f', start_row=15, dia=0, sistema='BI', analise='Cu')
+        # self.extrair_coluna(start_col_letter='f', start_row=20, dia=0, sistema='B', analise='Cu')
+
+        # self.extrair_coluna(start_col_letter='g', start_row=5, dia=0, sistema='AI', analise='Ni')
+        # self.extrair_coluna(start_col_letter='g', start_row=10, dia=0, sistema='A', analise='Ni')
+        # self.extrair_coluna(start_col_letter='g', start_row=15, dia=0, sistema='BI', analise='Ni')
+        # self.extrair_coluna(start_col_letter='g', start_row=20, dia=0, sistema='B', analise='Ni')
+
+        # self.extrair_coluna(start_col_letter='h', start_row=5, dia=0, sistema='AI', analise='Pb')
+        # self.extrair_coluna(start_col_letter='h', start_row=10, dia=0, sistema='A', analise='Pb')
+        # self.extrair_coluna(start_col_letter='h', start_row=15, dia=0, sistema='BI', analise='Pb')
+        # self.extrair_coluna(start_col_letter='h', start_row=20, dia=0, sistema='B', analise='Pb')
 
         self.extrair_coluna(start_col_letter='i', start_row=5, dia=0, sistema='AI', analise='Zn')
         self.extrair_coluna(start_col_letter='i', start_row=10, dia=0, sistema='A', analise='Zn')
         self.extrair_coluna(start_col_letter='i', start_row=15, dia=0, sistema='BI', analise='Zn')
         self.extrair_coluna(start_col_letter='i', start_row=20, dia=0, sistema='B', analise='Zn')
 
+
+
+        # Dia 14
+        # self.extrair_coluna(start_col_letter='j', start_row=5, dia=14, sistema='AI', analise='Cd')
+        # self.extrair_coluna(start_col_letter='j', start_row=10, dia=14, sistema='A', analise='Cd')
+        # self.extrair_coluna(start_col_letter='j', start_row=15, dia=14, sistema='BI', analise='Cd')
+        # self.extrair_coluna(start_col_letter='j', start_row=20, dia=14, sistema='B', analise='Cd')
+
+        # self.extrair_coluna(start_col_letter='k', start_row=5, dia=14, sistema='AI', analise='Cr')
+        # self.extrair_coluna(start_col_letter='k', start_row=10, dia=14, sistema='A', analise='Cr')
+        # self.extrair_coluna(start_col_letter='k', start_row=15, dia=14, sistema='BI', analise='Cr')
+        # self.extrair_coluna(start_col_letter='k', start_row=20, dia=14, sistema='B', analise='Cr')
+
+        # self.extrair_coluna(start_col_letter='l', start_row=5, dia=14, sistema='AI', analise='Cu')
+        # self.extrair_coluna(start_col_letter='l', start_row=10, dia=14, sistema='A', analise='Cu')
+        # self.extrair_coluna(start_col_letter='l', start_row=15, dia=14, sistema='BI', analise='Cu')
+        # self.extrair_coluna(start_col_letter='l', start_row=20, dia=14, sistema='B', analise='Cu')
+
+        # self.extrair_coluna(start_col_letter='m', start_row=5, dia=14, sistema='AI', analise='Ni')
+        # self.extrair_coluna(start_col_letter='m', start_row=10, dia=14, sistema='A', analise='Ni')
+        # self.extrair_coluna(start_col_letter='m', start_row=15, dia=14, sistema='BI', analise='Ni')
+        # self.extrair_coluna(start_col_letter='m', start_row=20, dia=14, sistema='B', analise='Ni')
+
+        # self.extrair_coluna(start_col_letter='n', start_row=5, dia=14, sistema='AI', analise='Pb')
+        # self.extrair_coluna(start_col_letter='n', start_row=10, dia=14, sistema='A', analise='Pb')
+        # self.extrair_coluna(start_col_letter='n', start_row=15, dia=14, sistema='BI', analise='Pb')
+        # self.extrair_coluna(start_col_letter='n', start_row=20, dia=14, sistema='B', analise='Pb')
+
+        self.extrair_coluna(start_col_letter='o', start_row=5, dia=14, sistema='AI', analise='Zn')
+        self.extrair_coluna(start_col_letter='o', start_row=10, dia=14, sistema='A', analise='Zn')
+        self.extrair_coluna(start_col_letter='o', start_row=15, dia=14, sistema='BI', analise='Zn')
+        self.extrair_coluna(start_col_letter='o', start_row=20, dia=14, sistema='B', analise='Zn')
+
+
+
+    def extrair_celula(self, start_col_letter: str, start_row: int,dia: int, sistema: str, analise: str, concentracao: int):
+        ncols = 1
+        nrows = 1
+
+        print(f"Extraindo coluna. Sistema: {sistema}. Análise: {analise}. Fungo: {self.fungo}", )
+        
+        ws = self.ws
+        # Convert start column (e.g., "D") to a number
+        start_col = column_index_from_string(start_col_letter)
+
+        # Compute end column and row
+        end_col = start_col + ncols - 1
+        end_row = start_row + nrows - 1
+
+        # Convert back to Excel letters
+        end_col_letter = get_column_letter(end_col)
+
+        # Build Excel-style range (e.g., "D5:F9")
+        cell_range = f"{start_col_letter}{start_row}:{end_col_letter}{end_row}"
+
+        # Extract data from that range
+
+        
+
+        for i, row in enumerate(ws[cell_range]):
+            for j, cell in enumerate(row):
+                
+                self.add_amostra(sistema=sistema, dia=dia, concentracao=concentracao, analise=analise, valor=cell.value)
+    
+    
     def extrair_coluna(self, start_col_letter: str, start_row: int,dia: int, sistema: str, analise: str):
         ncols = 1
         nrows = 5
 
-        print(f"Extraindo quadrante. Sistema: {sistema}. Análise: {analise}", )
+        print(f"Extraindo coluna. Sistema: {sistema}. Análise: {analise}. Fungo: {self.fungo}", )
         
         ws = self.ws
         # Convert start column (e.g., "D") to a number
@@ -102,13 +181,12 @@ class Extrator:
         # Extract data from that range
 
         concentracao_map = [0, 25, 50, 75, 100]
-        dia_map = [0, 7, 14]
         
 
         for i, row in enumerate(ws[cell_range]):
             for j, cell in enumerate(row):
                 
-                self.add_amostra(sistema=sistema, dia=dia_map[j], concentracao=concentracao_map[i], analise=analise, valor=cell.value)
+                self.add_amostra(sistema=sistema, dia=dia, concentracao=concentracao_map[i], analise=analise, valor=cell.value)
     
 
     def extrair_fisicoquimico(self):
@@ -147,7 +225,7 @@ class Extrator:
         ncols = 3
         nrows = 5
 
-        print(f"Extraindo quadrante. Sistema: {sistema}. Análise: {analise}", )
+        print(f"Extraindo quadrante. Sistema: {sistema}. Análise: {analise}. Fungo: {self.fungo}", )
         
         ws = self.ws
         # Convert start column (e.g., "D") to a number
